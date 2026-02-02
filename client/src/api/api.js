cc935ac05c868ecb3f6c0b43657a880a38260136
HuanaVu <huanvu210@gmail.com>
2026-02-02 20:27:22 +0700

    fix: Add Content-Type header to axios instance

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -4,6 +4,9 @@ const API_BASE_URL = 'http://localhost:5055/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 export const productsApi = {
